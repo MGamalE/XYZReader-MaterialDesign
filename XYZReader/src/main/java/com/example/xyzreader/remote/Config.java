d3afb7074db7170b0593bd5fd54265f675fb6040
MohamedGElsharkawy <31111546+MohamedGElsharkawy@users.noreply.github.com>
2018-02-14 07:14:19 +0200

    commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,23 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+    private static String TAG = Config.class.toString();
+
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/March/58c5d68f_xyz-reader/xyz-reader.json" );
+        } catch (MalformedURLException ignored) {
+            // TODO: throw a real error
+            Log.e(TAG, "Please check your internet connection.");
+        }
+
+        BASE_URL = url;
+    }
+}
